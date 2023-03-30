411a639713af90db9a70c20b784fd1dbc7f4406b
Maniusdezeb <103053114+Daan1254@users.noreply.github.com>
2023-03-30 10:02:17 +0200

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+  const config = new DocumentBuilder()
+    .setTitle('T-Agency API')
+    .setDescription('T-Agency API V1')
+    .setVersion('1.0')
+    .addTag('t-agency')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors()
+
   await app.listen(3000);
 }
 bootstrap();
